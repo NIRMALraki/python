2d673f44e2e47f3cecf4068c66ae5471e99dd3c2
Nirmal kumar <57279816+NIRMALraki@users.noreply.github.com>
2021-03-02 22:00:14 +0530

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,49 @@
+d={}
+class employee:
+    def calculate(self,name,address,pan,basic,tds,deduct,hra,da):
+        self.name=name
+        self.address=address
+        self.pan=pan
+        self.basic=basic
+        self.tds=tds
+        self.deduct=deduct
+        self.hra=hra*1.176
+        self.da=da*0.25
+        self.pf=3000
+        salcal=self.basic+self.hra+self.da-(self.pf+self.tds)
+        return salcal
+    def accept(self):
+        name=input("enter your name")
+        address=input("enter your address")
+        pan=input("enter your pan no")
+        basic=int(input("enter basic pay"))
+        tds=200
+        deduct=300
+        hra=67
+        da=78
+        self.sal=self.calculate(name,address,pan,basic,tds,deduct,hra,da)
+
+    def display(self):
+        print("name",self.name)
+        print("address",self.address)
+        print("pan",self.pan)
+        print("salary",self.sal)
+    
+    def search(self,name):
+        for k,v in d.items():
+            print("k==",k)
+            print("v==",v)
+            if k==name:
+                print(k.__dict__)
+
+e=employee()
+e.accept()
+
+print("displaying details")
+e.display()
+
+print ("searching")
+e.search("nirmal")
+
+
+            
